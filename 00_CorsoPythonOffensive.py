659f891ed2dded1cc5358793895aac48514fa02a
Roberto <stanzabarca@hotmail.com>
2024-02-10 11:48:47 +0100

    Add files via upload

---
+++ 00_CorsoPythonOffensive.py
@@ -0,0 +1,19 @@
+# Corso Python Offensive
+
+
+'''
+questo e' un commento
+'''
+print("Ciao a tutti 'ciao'")
+print(100)
+print("100 + 50")
+
+# Tipi di dati
+
+print(type("Testo")) # STRING
+print(type(100)) # INTEGER
+print(type(2.5)) # FLOAT
+print(type(True)) # BOOLEAN (Vero o falso)
+
+
+
