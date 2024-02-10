23594e81a5397e0fed7614d5061df6aec6bd45c7
Roberto <stanzabarca@hotmail.com>
2024-02-10 11:49:02 +0100

    Add files via upload

---
+++ 01_Variabili.py
@@ -0,0 +1,27 @@
+# VARIABILI
+
+scatola = ("Roberto")
+print(scatola)
+
+ip_address = ("192.168.1.0")
+print(type(ip_address))
+
+port = 80
+print(type(port))
+
+numero = float(4)
+print(type(numero))
+print(numero)
+
+var1 = 1
+var2 = 2
+var3 = 3
+
+tutte = (var1, var2, var3)
+print(len(tutte))
+
+nome = input("Quale e' il tuo nome?")
+eta = input("Quanti anni hai?")
+
+print(nome)
+print(eta)
