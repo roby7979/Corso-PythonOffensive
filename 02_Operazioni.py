e23c05e332778a5ba4799b3de60f09f6e26daadf
Roberto <stanzabarca@hotmail.com>
2024-02-10 11:49:18 +0100

    Add files via upload

---
+++ 02_Operazioni.py
@@ -0,0 +1,27 @@
+# OPERAZIONI
+
+print(2 + 3)
+print(8 - 4)
+print(2 * 4)
+print(10 / 2)
+
+print(2 ** 3)
+
+print("Ciao" + "Python")
+print("Ciao" + " " + "Python")
+print("Ciao " * 5)
+float = 2.5 * 2
+print("Ciao " * int(float))
+
+print(3 > 4)
+print(8 > 2)
+print(10 < 2)
+
+print(4 == 4)
+
+numero_1 = 12
+numero_2 = 7
+
+risultato = numero_1 + numero_2
+
+print(risultato)
