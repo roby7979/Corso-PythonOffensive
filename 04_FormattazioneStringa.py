51b8af8adae4b884a43e9551b31a012e0167cc38
Roberto <stanzabarca@hotmail.com>
2024-02-28 22:33:00 +0100

    Create 04_FormattazioneStringa.py

---
+++ 04_FormattazioneStringa.py
@@ -0,0 +1,10 @@
+name = "Roberto"
+rol = "programmatore"
+eta = 44
+print("Ciao il mio nome e' %s e sono un %s. Attualmente ho %d anni" % (name, rol, eta))
+
+print("Ciao sono {}!".format(name))
+print("Ciao sono {0}! e ho {1} anni, non scherzo sono {0}".format(name, eta))
+
+print(f"Ciao sono {name}!")
+print(f"Ciao sono {name}! e ho {eta} anni, non scherzo sono {name}")
